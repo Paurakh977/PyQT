162eafa4487fbe0939b3ee797a3d762ede851483
“Paurakh977” <pandeypaurakhraj@gmail.com>
2024-01-14 23:41:11 +0545

    upload_view done

--- upload_view_page.py
+++ upload_view_page.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'table.ui'
+# Form implementation generated from reading ui file '.\upload_view_page.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.10
 #
